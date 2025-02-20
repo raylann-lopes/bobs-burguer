9f35961ab36a33a1c38edea8cd22e4745117eade
raylannlopes <raylannlopes@gmail.com>
2025-02-19 21:57:14 -0300

    feat: implement restaurant menu page with consumption method options and header component

---
+++ src/app/[slug]/menu/page.tsx
@@ -0,0 +1,38 @@
+import { notFound } from "next/navigation";
+
+import { db } from "@/lib/prisma";
+
+import RestaurantHeader from "./components/header";
+
+interface RestaurantMenuPageProps {
+  params: Promise<{ slug: string }>;
+  searchParams: Promise<{ ConsumptionMethod: string }>;
+}
+
+const isConsumptionMethodValid = (consumptionMethod: string) => {
+  return ["DINE_IN", "TAKEAWAY"].includes(consumptionMethod.toUpperCase());
+};
+
+const RestaurantMenuPage = async ({
+  params,
+  searchParams,
+}: RestaurantMenuPageProps) => {
+  const { slug } = await params;
+  const { ConsumptionMethod } = await searchParams;
+  if (!isConsumptionMethodValid(ConsumptionMethod)) {
+    return notFound();
+  }
+  const restaurant = await db.restaurant.findUnique({
+    where: { slug },
+  });
+  if (!restaurant) {
+    return notFound();
+  }
+  return (
+    <div>
+      <RestaurantHeader restaurant={restaurant} />
+    </div>
+  );
+};
+
+export default RestaurantMenuPage;
