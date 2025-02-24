70a7750a2f1111bdd050fe69c12d1da329232b0d
raylannlopes <raylannlopes@gmail.com>
2025-02-23 21:36:26 -0300

    feat: enhance navigation by adding order links in product and restaurant headers

--- src/app/[slug]/menu/components/header.tsx
+++ src/app/[slug]/menu/components/header.tsx
@@ -3,7 +3,7 @@
 import { Restaurant } from "@prisma/client";
 import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 
 import { Button } from "@/components/ui/button";
 
@@ -12,14 +12,16 @@ interface RestaurantHeaderProps {
 }
 
 const RestaurantHeader = ({ restaurant }: RestaurantHeaderProps) => {
+  const { slug } = useParams<{ slug: string }>();
   const router = useRouter();
   const handleBackClick = () => router.back();
+  const handleOrdersClick = () => router.push(`/${slug}/orders`);
   return (
     <div className="relative h-[250px] w-full">
       <Button
         variant="secondary"
         size="icon"
-        className="rounded-full absolute top-4 left-4 z-50"
+        className="absolute left-4 top-4 z-50 rounded-full"
         onClick={handleBackClick}
       >
         <ChevronLeftIcon />
@@ -34,7 +36,8 @@ const RestaurantHeader = ({ restaurant }: RestaurantHeaderProps) => {
       <Button
         variant="secondary"
         size="icon"
-        className="rounded-full absolute top-4 right-4 z-50"
+        className="absolute right-4 top-4 z-50 rounded-full"
+        onClick={handleOrdersClick}
       >
         <ScrollTextIcon />
       </Button>
